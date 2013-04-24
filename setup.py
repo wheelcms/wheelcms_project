21416add52999a34949ad560b78f63e0c9f3bdfc
Ivo van der Wijk <github@in.m3r.nl>
2013-04-24 09:46:53 +0200

    copy base project setup

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='wheelcms_project',
+      version=version,
+      description="WheelCMS project package",
+      long_description=open("README.md").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Ivo van der Wijk',
+      author_email='wheelcms@in.m3r.nl',
+      url='http://github.com/wheelcms/wheelcms_project',
+      license='BSD',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=[],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'wheelcms_axle',
+          'setuptools',
+          'pytest',
+      ],
+      entry_points={
+      },
+
+      )
+
---
+++ wheelcms_project/settings/base/settings_database.py
@@ -0,0 +1,8 @@
+DEFAULT_DB = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'wheel.db'
+}
+
+DATABASES = {
+    'default': DEFAULT_DB
+}
