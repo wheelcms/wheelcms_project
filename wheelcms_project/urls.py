3c92e68b92f55bfb1ca3cbd9ddc8b44d68360c55
Ivo van der Wijk <github@in.m3r.nl>
2013-04-24 10:11:57 +0200

    add base project url config

---
+++ wheelcms_project/urls.py
@@ -0,0 +1,30 @@
+from django.contrib import admin
+from django.conf.urls.defaults import patterns, include
+
+from django.conf import settings
+
+
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+admin.autodiscover()
+
+urlpatterns = staticfiles_urlpatterns()
+
+## or import static_urls; urlpatterns = static_urls.urlpatterns
+
+urlpatterns += patterns('',
+    (r'^admin/', include(admin.site.urls)),
+    (r'^favicon.ico$', 'django.views.generic.simple.redirect_to',
+                        {'url': '/static/images/favicon.ico'}),
+    (r'^tinymce/', include('tinymce.urls')),
+    (r'^accounts/', include('userena.urls')),
+    (r'^search/', include('haystack.urls')),
+    (r'', include('wheelcms_axle.urls')),
+)
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve',
+         {'document_root': settings.MEDIA_ROOT}),
+    )
+
