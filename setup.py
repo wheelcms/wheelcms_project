358c458f1c2ce668f33096df1979ac227b38199e
Ivo van der Wijk <github@in.m3r.nl>
2013-05-17 15:25:45 +0200

    support url-based database configuration

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setup(name='wheelcms_project',
       include_package_data=True,
       zip_safe=False,
       install_requires=[
+          'dj-database-url',
           'wheelcms_axle',
           'setuptools',
           'pytest',
