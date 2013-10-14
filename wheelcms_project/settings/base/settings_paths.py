3b205f52a3190b2784a26556adf63c3144de2bc9
Ivo van der Wijk <github@in.m3r.nl>
2013-10-14 11:27:03 +0200

    attempt to provide reasonable defaults for STATIC/MEDIA root

--- wheelcms_project/settings/base/settings_paths.py
+++ wheelcms_project/settings/base/settings_paths.py
@@ -1,5 +1,9 @@
 import os
-BASE=os.path.join(os.path.dirname(__file__), "..", "..")
+from .util import get_env_variable
+
+BASE = os.path.join(os.path.dirname(__file__), "..", "..")
+
+BASE = get_env_variable('PROJECT_HOME', BASE)
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
@@ -14,7 +18,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(BASE, "..", "static")
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
