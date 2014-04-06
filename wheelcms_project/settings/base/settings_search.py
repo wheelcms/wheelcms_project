ab8b1644c258b44151feeb97eda466b40619a6cc
Ivo van der Wijk <github@in.m3r.nl>
2014-04-06 21:19:26 +0200

    use index collector workaround

--- wheelcms_project/settings/base/settings_search.py
+++ wheelcms_project/settings/base/settings_search.py
@@ -3,7 +3,8 @@ from .util import get_env_variable
 HAYSTACK_CONNECTIONS = {
 
     'default': {
-        'ENGINE': 'haystack.backends.solr_backend.SolrEngine',
+        # 'ENGINE': 'haystack.backends.solr_backend.SolrEngine',
+        'ENGINE': 'wheelcms_axle.solr_engine.SolrEngine',
         'URL': get_env_variable('SOLR_URL', 'http://127.0.0.1:8983/solr'),
         'INCLUDE_SPELLING': True,
 
